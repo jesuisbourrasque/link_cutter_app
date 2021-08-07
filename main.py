6118786bac4671e76cda6c0f87389cdf5d5f0206
Bourrasque <jesuisbourrasque@gmail.com>
2021-08-08 00:06:39 +0300

    code refactoring

--- main.py
+++ main.py
@@ -9,34 +9,34 @@ database = DB()
 
 def main():
     args = parse_args(sys.argv[1:])
-    long_url = args.url
+    url = args.url
     generate = args.generate
     short_url = args.short_url
     with database as db:
         db.create_schema()
         if generate:
             try:
-                database.add_long_url(long_url)
+                database.add_long_url(url)
             except Exception:
                 pass
             if short_url:
                 try:
                     database.add_short_url(short_url)
-                    print(f'Короткая ссылка на вашу {short_url}')
+                    print(f'Короткая ссылка на вашу {short_url}2')
                 except Exception:
                     print(f'Такая короткая ссылка уже существует: {short_url}')
             else:
                 try:
-                    short_url = shortuuid.uuid(long_url)
+                    short_url = shortuuid.uuid(url)
                     database.add_short_url(short_url)
+                    print(f'Короткая ссылка на вашу {short_url}1')
                 except Exception:
-                    pass
-                finally:
-                    print(f'Короткая ссылка на вашу {short_url}')
-        if not generate and long_url:
-            long_url = database.select_long_url(long_url)
+                    print('Fuck')
+        if not generate and url:
+            long_url = database.select_long_url(url)
+            print(long_url)
             if long_url:
-                print(long_url)
+                print(url)
             else:
                 print('Такой ссылки нет')
 
