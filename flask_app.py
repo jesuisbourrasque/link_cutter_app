c3102f4a1f4ce8eba92a33b1d9811612f5ea20fe
Bourrasque <jesuisbourrasque@gmail.com>
2021-08-25 00:16:19 +0300

    code refactoring

--- flask_app.py
+++ flask_app.py
@@ -40,9 +40,4 @@ def get_link(link):
         ), 404
 
 
-if __name__ == "__main__":
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.debug = True
-    app.run()
-
+app.run(debug=True, host='0.0.0.0')
